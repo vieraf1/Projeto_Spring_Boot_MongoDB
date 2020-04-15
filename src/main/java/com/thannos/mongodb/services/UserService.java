bcc6f55c0cf963dba1122d36bdaf71389ea92bbd
Lucas Vieira <vieraf1@hotmail.com.br>
2020-04-14 23:10:16 -0300

    Find User by Id

--- src/main/java/com/thannos/mongodb/services/UserService.java
+++ src/main/java/com/thannos/mongodb/services/UserService.java
@@ -1,12 +1,14 @@
 package com.thannos.mongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.thannos.mongodb.domain.User;
 import com.thannos.mongodb.repositories.UserRepository;
+import com.thannos.mongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,11 @@ public class UserService {
 
 	public List<User> findAll() {
 		return repository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 	
 }
---
+++ src/main/java/com/thannos/mongodb/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,10 @@
+package com.thannos.mongodb.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+}
