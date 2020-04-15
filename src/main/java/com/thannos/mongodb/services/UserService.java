ad7bcb9c93b7f3aa9e6b901d4f5589f83cf601db
Lucas Vieira <vieraf1@hotmail.com.br>
2020-04-14 23:43:56 -0300

    Update User

--- src/main/java/com/thannos/mongodb/services/UserService.java
+++ src/main/java/com/thannos/mongodb/services/UserService.java
@@ -35,6 +35,17 @@ public class UserService {
 		repository.deleteById(id);
 	}
 	
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repository.save(newObj);
+	}
+	
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
+
 	public User fromDTO(UserDTO userDto) {
 		return new User(userDto.getId(), userDto.getName(), userDto.getEmail());
 	}
