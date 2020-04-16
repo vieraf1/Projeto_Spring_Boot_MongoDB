7e3f32c7529804b583ed2582ce25ab3efa32fdf9
Lucas Vieira <vieraf1@hotmail.com.br>
2020-04-15 23:43:10 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/thannos/mongodb/resources/UserResource.java
+++ src/main/java/com/thannos/mongodb/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.thannos.mongodb.domain.Post;
 import com.thannos.mongodb.domain.User;
 import com.thannos.mongodb.dto.UserDTO;
 import com.thannos.mongodb.services.UserService;
@@ -61,5 +62,11 @@ public class UserResource {
 		obj.setId(id);
 		service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
