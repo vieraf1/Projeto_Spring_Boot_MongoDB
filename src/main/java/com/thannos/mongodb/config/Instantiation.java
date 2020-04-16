c0fa09ebef2e3d3dc7e5de4412e4ed0520427771
Lucas Vieira <vieraf1@hotmail.com.br>
2020-04-16 00:06:25 -0300

    Add Comments in Post

--- src/main/java/com/thannos/mongodb/config/Instantiation.java
+++ src/main/java/com/thannos/mongodb/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.thannos.mongodb.domain.Post;
 import com.thannos.mongodb.domain.User;
 import com.thannos.mongodb.dto.AuthorDTO;
+import com.thannos.mongodb.dto.CommentDTO;
 import com.thannos.mongodb.repositories.PostRepository;
 import com.thannos.mongodb.repositories.UserRepository;
 
@@ -40,6 +41,15 @@ public class Instantiation implements CommandLineRunner {
 		
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
+		CommentDTO comment1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO comment2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		
+		post1.getComments().addAll(Arrays.asList(comment1, comment2));
+		
+		CommentDTO comment3 = new CommentDTO("Tenha um ótimo dia", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		
+		post2.getComments().add(comment3);
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
