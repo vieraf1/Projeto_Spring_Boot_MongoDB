98c683031156a55632c7338b9756ce11b7b0e898
Lucas Vieira <vieraf1@hotmail.com.br>
2020-04-16 23:44:16 -0300

    Consulta simples com @Query

--- src/main/java/com/thannos/mongodb/repositories/PostRepository.java
+++ src/main/java/com/thannos/mongodb/repositories/PostRepository.java
@@ -3,12 +3,16 @@ package com.thannos.mongodb.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.thannos.mongodb.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> findByTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 
