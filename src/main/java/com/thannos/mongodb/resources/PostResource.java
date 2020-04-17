bac832a3424a2279a7e5cad4653b895b8c5a5f56
Lucas Vieira <vieraf1@hotmail.com.br>
2020-04-16 23:32:55 -0300

    Consulta simples com query methods

--- src/main/java/com/thannos/mongodb/resources/PostResource.java
+++ src/main/java/com/thannos/mongodb/resources/PostResource.java
@@ -1,13 +1,17 @@
 package com.thannos.mongodb.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.thannos.mongodb.domain.Post;
+import com.thannos.mongodb.resources.util.URL;
 import com.thannos.mongodb.services.PostService;
 
 @RestController
@@ -23,4 +27,9 @@ public class PostResource {
 		return ResponseEntity.ok().body(obj);
 	}
 
+	@GetMapping(value = "/titlesearch")
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+		List<Post> posts = service.findByTitle(URL.decodeParam(text));
+		return ResponseEntity.ok().body(posts);
+	}
 }
